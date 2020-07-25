75604e732d54dea4df366a3ff4ccff311b52b31f
Astemir Eleev <astemireleev@gmail.com>
2020-07-25 13:58:53 +0300

    Initial Commit

---
+++ T2iles/T2ilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  T2ilesApp.swift
+//  T2iles
+//
+//  Created by Astemir Eleev on 25.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct T2ilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ T2ilesTests/T2ilesTests.swift
@@ -0,0 +1,33 @@
+//
+//  T2ilesTests.swift
+//  T2ilesTests
+//
+//  Created by Astemir Eleev on 25.07.2020.
+//
+
+import XCTest
+@testable import T2iles
+
+class T2ilesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
